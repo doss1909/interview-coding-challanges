5e71853cf105657228bbd753e581c57de5c28d54
prabhudasu <prabhudasu190@gmail.com>
2026-03-10 22:14:21 +0530

    prime number problem solved

--- src/numberChallenges/PrimeNumOrNot.java
+++ src/numberChallenges/PrimeNumOrNot.java
@@ -1,9 +1,15 @@
 package numberChallenges;
 
 public class PrimeNumOrNot {
-
+    public boolean isPrimeNum(int num){
+        for (int i = 2; i < num/2; i++) {
+            if(num%i == 0) return false;
+        }
+        return true;
+    }
     public static void main(String[] args) {
-
+        PrimeNumOrNot obj = new PrimeNumOrNot();
+        System.out.println(obj.isPrimeNum(9));
     }
 
 }
