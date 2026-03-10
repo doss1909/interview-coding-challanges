ddea07bd1e31f8a6aadacfc5970cbd02eca4ebc3
prabhudasu <prabhudasu190@gmail.com>
2026-03-10 23:03:05 +0530

    optimized solution for fibonacci with HashMap

--- src/numberChallenges/FibonacciSeriesUsingRecursion.java
+++ src/numberChallenges/FibonacciSeriesUsingRecursion.java
@@ -1,5 +1,7 @@
 package numberChallenges;
 
+import java.util.HashMap;
+
 public class FibonacciSeriesUsingRecursion {
 
     public int fibonacciSeriesUsingRecursion(int num){
@@ -7,11 +9,43 @@ public class FibonacciSeriesUsingRecursion {
 
         return fibonacciSeriesUsingRecursion(num-1)+fibonacciSeriesUsingRecursion(num-2);
     }
+    /*
+    fibonacci(5)
+ ├── fibonacci(4)
+ │    ├── fibonacci(3)
+ │    │    ├── fibonacci(2)
+ │    │    │    ├── fibonacci(1) = 1
+ │    │    │    └── fibonacci(0) = 0
+ │    │    └── fibonacci(1) = 1
+ │    └── fibonacci(2)
+ │         ├── fibonacci(1)
+ │         └── fibonacci(0)
+ └── fibonacci(3)
+    Time Complexity: O(2^n)
+    Space Complexity: O(n) (recursion stack)*/
+
+    //optimized answer
+    static HashMap<Integer, Integer> map = new HashMap<>();
+
+    public static int fibonacciOptimized(int n) {
+
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        if (map.containsKey(n))
+            return map.get(n);
+
+        int result = fibonacciOptimized(n - 1) + fibonacciOptimized(n - 2);
+        map.put(n, result);
+
+        return result;
+    }
 
     public static void main(String[] args) {
         FibonacciSeriesUsingRecursion obj = new FibonacciSeriesUsingRecursion();
-        for (int i = 0; i < 10; i++) {
-            System.out.print(obj.fibonacciSeriesUsingRecursion(i)+" ");
+        for (int i = 0; i < 9; i++) {
+            //System.out.print(obj.fibonacciSeriesUsingRecursion(i)+" ");
+            System.out.print(obj.fibonacciOptimized(i)+" ");
         }
     }
 
